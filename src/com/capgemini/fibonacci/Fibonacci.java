abd0cfdc39369f84b0c07fad382fc67856d6313e
Przemysław Wojtków <przemyslaw.wojtkow@capgemini.com>
2016-07-12 22:44:25 +0200

    First commit, with complete, working app

---
+++ src/com/capgemini/fibonacci/Fibonacci.java
@@ -0,0 +1,26 @@
+package com.capgemini.fibonacci;
+
+/**
+ * @author PWOJTKOW
+ *
+ *
+ *         F1 F2 F3 F4 F5 F6 F7 F8 F9 1 1 2 3 5 8 13 21 34
+ * 
+ *         F10 F11 F12 F13 F14 F15 F16 F17 F18 F19 55 89 144 233 377 610 987
+ *         1597 2584 4181 …
+ */
+public class Fibonacci {
+
+	public static long fib(int n) {
+		if (n < 0)
+			throw new IllegalArgumentException("Input value below zero");
+		else if(n>18)
+			throw new IllegalArgumentException("Input value too high");
+		
+		if (n == 1 || n == 2)
+			return 1;
+		else if (n > 2)
+			return fib(n - 2) + fib(n - 1);
+		return 0;
+	}
+}
---
+++ src/com/capgemini/fibonacci/test/FibonacciTest.java
@@ -0,0 +1,87 @@
+package com.capgemini.fibonacci.test;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import com.capgemini.fibonacci.Fibonacci;
+
+public class FibonacciTest {
+
+	@Rule
+	public ExpectedException expected = ExpectedException.none();
+
+	@Test
+	public void shouldReturnOneInputOne() {
+		// given
+		int n = 1;
+
+		// when
+		long fibonacciValue = Fibonacci.fib(n);
+
+		// then
+		org.junit.Assert.assertEquals(1, fibonacciValue);
+	}
+
+	@Test
+	public void shouldReturnOneInputTwo() {
+		// given
+		int n = 2;
+
+		// when
+		long fibonacciValue = Fibonacci.fib(n);
+
+		// then
+		org.junit.Assert.assertEquals(1, fibonacciValue);
+	}
+
+	@Test
+	public void shouldReturnEight() {
+		// given
+		int n = 6;
+
+		// when
+		long fibonacciValue = Fibonacci.fib(n);
+
+		// then
+		org.junit.Assert.assertEquals(8, fibonacciValue);
+	}
+
+	@Test
+	public void shouldReturnOne() {
+		// given
+		int n = 0;
+
+		// when
+		long fibonacciValue = Fibonacci.fib(n);
+
+		// then
+		org.junit.Assert.assertEquals(0, fibonacciValue);
+	}
+
+	@Test
+	public void souldThrowExceptionBelowZero() {
+		expected.expect(IllegalArgumentException.class);
+		expected.expectMessage("Input value below zero");
+		// given
+		
+		int n = -2;
+
+		// when
+		long fibonacciValue = Fibonacci.fib(n);
+	}
+	
+	@Test
+	public void shouldThrowExceptionUnder18(){
+		expected.expect(IllegalArgumentException.class);
+		expected.expectMessage("Input value too high");
+		//given
+		
+		int n = 20;
+		
+		//when
+		long fibonacciValue = Fibonacci.fib(n);
+		
+	}
+
+}
